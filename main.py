9d92130ede38c8303c71f67852f65d02bbf16c01
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-10-06 17:03:57 +0900

    AuthorizationPlus

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+import requests
+from fastapi import FastAPI, HTTPException
 from starlette.requests import Request
 
 app = FastAPI()
+
+TENANT = 'reud-mi'  # ここは自分のテナント名を入れてください
 
 
 @app.get("/")
@@ -9,8 +12,17 @@ def read_root():
     return {"200": "Welcome To Heroku"}
 
 
-@app.get("/himitu")
-def read_item(request: Request):
+@app.get("/himitu/{user_id}")
+def read_item(user_id: str, request: Request):
     k = request.headers.get('Authorization')
-    _ , script = k.split()
-    return {"Authorization Header": script}
+    if not k:
+        raise HTTPException(status_code=403, detail="forbidden")
+    headers = {'Authorization': k}
+    # 適宜読み替えてください
+    r = requests.get(f'https://{TENANT}.auth0.com/api/v2/users/{user_id}', headers=headers)
+    j = r.json()
+    t = r.text()
+    try:
+        return j
+    except:
+        return t

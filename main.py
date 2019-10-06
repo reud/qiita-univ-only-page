8b4f576360cd4d7e7f35f4be06be5aaa397e15a2
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-10-06 16:04:24 +0900

    AuthorizationPlus

--- main.py
+++ main.py
@@ -9,21 +9,7 @@ def read_root():
     return {"200": "Welcome To Heroku"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.get("/fizz_buzz/{num}")
-def read_item(num: int):
-    # https: // ja.wikipedia.org / wiki / Fizz_Buzz
-    if not num % 15:
-        return {num: "Fizz Buzz"}
-    elif not num % 5 or not num % 3:
-        return {num: 'Fizz' if not num % 3 else 'Buzz'}
-    else:
-        return {num: 'Stay Silent'}
-
 @app.get("/himitu")
 def read_item(item_id: int,request: Request):
-    return {"item_id": item_id, "q": q}
+    k = request.headers.get('Authorization')
+    return {"item_id": item_id, "Authorization Header": k}

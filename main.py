31b3ba2181679ff51aac1d41fbe5531e38910d82
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-10-09 18:13:27 +0900

    add CORS

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import traceback
 import requests
 from fastapi import FastAPI, HTTPException
 from starlette.requests import Request
+from starlette.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
 
 @app.get("/")
 def read_root():
